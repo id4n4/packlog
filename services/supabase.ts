346cdef3702327bd2fb87483904674cd500be3e8
idan <nagatiitoo@hotmail.com>
2026-05-11 21:11:08 +0200

    feat: reset project structure and remove unused files
    - Deleted the reset-project.js script to streamline project setup.
    - Removed supabase.ts as it was no longer needed.
    - Added App.css for styling the application.
    - Created App.tsx as the main application component.
    - Included hero image and logos for React and Vite.
    - Introduced index.css for global styles and theming.
    - Set up main.tsx as the entry point for the React application.
    - Configured TypeScript with tsconfig.app.json and tsconfig.node.json for better type checking and module resolution.
    - Added vite.config.ts for Vite configuration.

--- services/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.EXPO_PUBLIC_API_URL_SUPABASE!;
-const supabaseKey = process.env.EXPO_PUBLIC_API_KEY_SUPABASE!;
-
-console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Key:", supabaseKey);
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
